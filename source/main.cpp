0d7e1bde4332be275f768bb4681a78b6eaed8bb1
Aaron Jacobs <aaron@aaronmjacobs.com>
2016-03-08 00:00:26 -0800

    Add GLFW to CMakeLists, update program

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,14 @@
+#include <GLFW/glfw3.h>
+
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-   printf("InitGL: simple program is running!\n");
+   if (glfwInit()) {
+      printf("InitGL: got GLFW initialized! :D\n");
+      glfwTerminate();
+   } else {
+      printf("InitGL: unable to initialize GLFW :(\n");
+   }
 
    return 0;
 }
