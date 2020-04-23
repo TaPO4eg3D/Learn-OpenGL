6d5efaa6739a41545bd51cfd182ce0e54a9c32c8
Denis Fomin <dfomin@retechlabs.com>
2020-04-24 01:20:43 +0700

    Holy triangle

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <fstream>
 
 GLFWwindow *createWindow() {
     GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
@@ -14,9 +16,54 @@ GLFWwindow *createWindow() {
 }
 
 void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
+}
+
+std::string loadShaderSource(const std::string &shaderFileName) {
+    std::string shaderFolderName = "source/shaders";
+
+    std::string fullPath = "./" + shaderFolderName + "/" + shaderFileName + ".shader";
+
+    std::ifstream stream(fullPath);
+    if (!stream.is_open()) {
+        std::cout << fullPath << std::endl;
+        std::cout << "Failed to open the shader!" << std::endl;
+    } else {
+        std::string line;
+        std::string content;
+
+        while (!stream.eof()) {
+            getline(stream, line);
+            content += line + "\n";
+        }
+
+        stream.close();
+        return content;
+    }
+}
+
+unsigned int loadShader(unsigned int shaderType, const std::string &shaderName) {
+    unsigned int shader;
+    shader = glCreateShader(shaderType);
+
+    std::string shaderSourceString = loadShaderSource(shaderName);
+    const char *shaderSource = shaderSourceString.c_str();
+
+    glShaderSource(shader, 1, &shaderSource, nullptr);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[522];
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::" << shaderName << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    return shader;
 }
 
 int main() {
@@ -27,7 +74,7 @@ int main() {
 
     // Initialize window and the OpenGL functions linking
     GLFWwindow *window = createWindow();
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
@@ -39,12 +86,79 @@ int main() {
         glViewport(0, 0, width, height);
     });
 
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f,
+    };
+
+    // Generate Vertex Buffer
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Load and compile Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = loadShader(GL_VERTEX_SHADER, "vertex");
+
+    // Load and compile Fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = loadShader(GL_FRAGMENT_SHADER, "fragment");
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[522];
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::PROGRAM::LINKING_ERROR\n" << infoLog << std::endl;
+    }
+
+    glUseProgram(shaderProgram);
+
+    // We already loaded them and running, so we don't need to store them anymore
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glUseProgram(shaderProgram);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. then set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     // Keep window nice and open
-    while(!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
